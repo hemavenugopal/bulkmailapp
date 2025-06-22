3e2b6534fde8d895ba3c8b5c0266edc230ae965b
Hema Venugopal <archuhema12@gmail.com>
2025-06-22 15:35:56 +0530

    adding changes

---
+++ backend/index.js
@@ -0,0 +1,81 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+// require("dotenv").config();
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const nodemailer = require("nodemailer");
+
+mongoose.connect("mongodb+srv://Hema:12345@cluster0.coatkch.mongodb.net/passkeyretryWrites=true&w=majority&appName=Cluster0").then(function () {
+        console.log("DB connected")
+    }).catch(function () {
+        console.log("Failed to DB")
+    })
+
+const credentials = mongoose.model("credential", {}, "bulkmail")
+
+
+
+
+
+
+
+
+
+app.post("/sendemail", function (req, res) {
+
+    let msg = req.body.msg
+let email = req.body.emailList
+
+    credentials.find().then(function (data) {
+        console.log(data)
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: data[0].toJSON().user,
+                pass: data[0].toJSON().pass
+            },
+        });
+
+        new Promise(async function (resolve, reject) {
+            try {
+                for (var i = 0; i < email.length; i++) {
+                    await transporter.sendMail({
+                        from: "archuhema12@gmail.com",
+                        to: email[i],
+                        subject: "A Message from BulkMail App",
+                        text: msg
+                    });
+
+                    console.log("Email sent to:" + email[i])
+                }
+                resolve("Success")
+            } catch (error) {
+                reject("Failed")
+            }
+        }).then(function () {
+            res.send(true)
+        }).catch(function () {
+            res.send(false)
+        })
+    }).catch(function (error) {
+        console.log(error)
+    })
+
+
+});
+
+
+
+
+app.listen(5000, function () {
+    console.log("Server Started...")
+})
+ 
+
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import axios from 'axios'
+import * as  XLSX from 'xlsx'
+
+
+function App() {
+  const[msg, setMsg] = useState("")
+  const[status, setStatus] = useState(false)
+  const[emailList, setEmailList] = useState([])
+    function handleMsg(event){
+    setMsg(event.target.value)
+  }
+  function send(){
+    setStatus(true)
+    axios.post("http://localhost:5000/sendemail",{msg:msg,emailList:emailList})
+    .then(function(data){
+      if(data.data === true){
+        alert("Email sent successfully")
+        setStatus(false)
+      }
+      else{
+        alert("Failed")
+        setStatus(false)
+      }
+
+    })
+    }
+    function handleFile(event){
+       const file = event.target.files[0]
+    const reader = new FileReader()
+
+    reader.onload = function(event){
+        const data = event.target.result
+        const workbook = XLSX.read(data,{type:"binary"})
+        const sheetName = workbook.SheetNames[0]
+        const worksheet = workbook.Sheets[sheetName]
+        const emailList = XLSX.utils.sheet_to_json(worksheet,{header:'A'})
+        const totalEmail = emailList.map((item)=>{
+          return (item.A)
+        })
+        console.log(totalEmail);
+        setEmailList(totalEmail)
+        
+    }
+
+    reader.readAsBinaryString(file)
+
+    }
+      
+    
+  
+  return (
+    <div>
+    <div className='bg-blue-950 text-white text-center'>
+      <h1 className='text-2xl font-medium px-5 py-3'>BulkMail</h1>
+    </div>
+     <div className='bg-blue-800 text-white text-center'>
+      <h1 className='font-medium px-5 py-3'>We can help your business with sending multiple emails at once</h1>
+    </div>
+    <div className='bg-blue-600 text-white text-center'>
+      <h1 className='font-medium px-5 py-3'>Drag and Drop</h1>
+    </div>
+    <div className='bg-blue-400 flex flex-col text-black items-center px-5 py-3'>
+      <textarea onChange={handleMsg} value={msg} className='w-[80%] h-32 py-2 outline-none px-2 border border-black rounded-md' placeholder='Enter your email...'></textarea>
+     <div >
+      <input type='file' onChange={handleFile} className='border-4 border-dashed px-4 py-4 mt-5 mb-5'/>
+    </div>
+    <p>Total emails in the file: {emailList.length}</p>
+    <button onClick={send} className='bg-blue-950 text-white px-2 py-2 rounded-md font-medium mt-2'>{status?"Sending...":"Send"}</button>
+    </div>
+   
+    
+    
+      <div className='bg-blue-300 text-white text-center p-8'>
+      
+    </div>
+      <div className='bg-blue-200 text-white text-center p-8'>
+      
+    </div>
+    </div>
+  )
+}
+
+export default App
